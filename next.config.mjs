0f1ad9447011fa7f97503a32a3de51e9bd058216
ivan horomanski <idhard@gmail.com>
2024-01-21 20:56:42 +0200

    fixing build

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,16 @@ import "./src/env.mjs";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
   // useSearchParams should be wrapped with a Suspense boundary https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
-  missingSuspenseWithCSRBailout: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 export default config;
